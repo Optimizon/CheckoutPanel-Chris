ae717c0005b00d6a6198a3774c509d46342899f5
Matthew Lee <Mattminwoolee@Matthews-MacBook-Pro-3.local>
2018-09-19 10:21:01 -0700

    Made final styling commits

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Checkout from './Checkout.jsx';
 
-ReactDOM.render(<Checkout />, document.getElementById('checkout'));
+// ReactDOM.render(<Checkout />, document.getElementById('checkout'));
 
-// window.Checkout = Checkout;
+window.Checkout = Checkout;
